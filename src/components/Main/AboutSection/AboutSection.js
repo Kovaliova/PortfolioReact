97af3c46a89a9562ba2c80012c295803c5fcec5c
Kate Kovaliova <kovaliova@belvg.com>
2025-01-23 15:08:19 +0100

    refresh portfolio

--- src/components/Main/AboutSection/AboutSection.js
+++ src/components/Main/AboutSection/AboutSection.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { string } from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ReactDownloadLink from 'react-download-link';
+import { saveAs } from 'file-saver';
 
 const AboutSection = ({ headingText, descriptionText, textCV, resumeLink, iconCV }) => {
   
+
   const handleDownload = () => {
     if (resumeLink) {
-      const link = document.createElement('a');
-      link.href = resumeLink;
-      link.download = "Kate_Kovaliova_CV.pdf";
-      link.target = "_blank";
-      link.rel = "noopener noreferrer";
-      link.click();
+      saveAs(resumeLink, 'Kate_Kovaliova_CV.pdf');
     }
   };
 
 
   const downloadComponent = resumeLink ? (
-    <ReactDownloadLink
-      label={textCV}
-      filename="Kate_Kovaliova_CV.pdf" 
-      exportFile={() => resumeLink}
-    />
+    <button onClick={handleDownload} className="link_resume">
+      <FontAwesomeIcon className="fa-icon" icon={iconCV} />
+      {textCV}
+    </button>
   ) : (
     <button onClick={handleDownload} className="link_resume">
       <FontAwesomeIcon className="fa-icon" icon={iconCV} />
