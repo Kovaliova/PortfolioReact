110288ce6d54764b7de90291ab4a1e01a73b4619
Kate Kovaliova <kovaliova@belvg.com>
2025-01-23 15:50:27 +0100

    refresh portfolio

--- src/components/Main/AboutSection/AboutSection.js
+++ src/components/Main/AboutSection/AboutSection.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { string } from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ReactDownloadLink from 'react-download-link';
 
-const AboutSection = ({ headingText, descriptionText, textCV, resumeLink, iconCV }) => {
-  
+const AboutSection = ({ headingText, descriptionText, textCV, resumeLink}) => {
+
   const handleDownload = () => {
     if (resumeLink) {
       const link = document.createElement('a');
@@ -16,42 +13,19 @@ const AboutSection = ({ headingText, descriptionText, textCV, resumeLink, iconCV
     }
   };
 
-
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent); 
-
-  const downloadComponent = resumeLink ? (
-    isMobile ? (
-      <button onClick={handleDownload} className="link_resume">
-        <FontAwesomeIcon className="fa-icon" icon={iconCV} />
-        {textCV}
-      </button>
-    ) : (
-
-      <ReactDownloadLink
-        label={textCV}
-        filename="Kate_Kovaliova_CV.pdf"
-        exportFile={() => resumeLink}
-      />
-    )
-  ) : (
-    <p>No resume available.</p>
-  );
-
   return (
     <div className="about-block--container">
       <h1 className="head_text label">{headingText}</h1>
       <p className="description_text">{descriptionText}</p>
-      {downloadComponent}
+      {resumeLink ? (
+        <button onClick={handleDownload} className="link_resume">
+          {textCV}
+        </button>
+      ) : (
+        <p>No resume available.</p>
+      )}
     </div>
   );
-};
-
-AboutSection.propTypes = {
-  headingText: string.isRequired,
-  descriptionText: string.isRequired,
-  resumeLink: string, 
-  textCV: string.isRequired,
-  iconCV: string.isRequired
 };
 
 export default AboutSection;
