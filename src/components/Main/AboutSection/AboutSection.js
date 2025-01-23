7e00b2c9b37e6e15f8fc342dd58ace65eab3ad2d
Kate Kovaliova <kovaliova@belvg.com>
2025-01-23 15:19:39 +0100

    refresh portfolio

--- src/components/Main/AboutSection/AboutSection.js
+++ src/components/Main/AboutSection/AboutSection.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { string } from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { saveAs } from 'file-saver';
+import ReactDownloadLink from 'react-download-link';
 
 const AboutSection = ({ headingText, descriptionText, textCV, resumeLink, iconCV }) => {
   
-
   const handleDownload = () => {
     if (resumeLink) {
-      saveAs(resumeLink, 'Kate_Kovaliova_CV.pdf');
+      const link = document.createElement('a');
+      link.href = resumeLink;
+      link.download = "Kate_Kovaliova_CV.pdf";
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
+      link.click();
     }
   };
 
 
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent); 
+
   const downloadComponent = resumeLink ? (
-    <button onClick={handleDownload} className="link_resume">
-      <FontAwesomeIcon className="fa-icon" icon={iconCV} />
-      {textCV}
-    </button>
+    isMobile ? (
+      <button onClick={handleDownload} className="link_resume">
+        <FontAwesomeIcon className="fa-icon" icon={iconCV} />
+        {textCV}
+      </button>
+    ) : (
+
+      <ReactDownloadLink
+        label={textCV}
+        filename="Kate_Kovaliova_CV.pdf"
+        exportFile={() => resumeLink}
+      />
+    )
   ) : (
-    <button onClick={handleDownload} className="link_resume">
-      <FontAwesomeIcon className="fa-icon" icon={iconCV} />
-      {textCV}
-    </button>
+    <p>No resume available.</p>
   );
 
   return (
